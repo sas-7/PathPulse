f429d8545262b959852d8d5ac9ae302dedb2eb2e
Saurabh Singh <131347866+sas-7@users.noreply.github.com>
2024-02-18 19:34:29 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,140 +1,3 @@
-// import React from "react";
-// import "./footer.css";
-// import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
-// import { Link } from "react-router-dom";
-// import logo from "../../assets/images/logo.png";
-
-// const quick__links = [
-//   {
-//     path: "/home",
-//     display: "Home",
-//   },
-//   {
-//     path: "/about",
-//     display: "About",
-//   },
-//   {
-//     path: "/tours",
-//     display: "Tours",
-//   },
-// ];
-
-// const quick__links2 = [
-//   {
-//     path: "/gallery",
-//     display: "Gallery",
-//   },
-//   {
-//     path: "/login",
-//     display: "Login",
-//   },
-//   {
-//     path: "/register",
-//     display: "Register",
-//   },
-// ];
-
-// const Footer = () => {
-//   return (
-//     <footer className="footer">
-//       <Container>
-//         <Row>
-//           <Col lg="3">
-//             <div className="logo">
-//               <img src={logo} alt="" />
-//               <p>Connect with us on social sites</p>
-//               <div className="social__link d-flex align-items-center gap-4">
-//                 <span>
-//                   <Link to="#">
-//                     <i class="ri-youtube-line"></i>
-//                   </Link>
-//                 </span>
-//                 <span>
-//                   <Link to="#">
-//                     <i class="ri-github-fill"></i>
-//                   </Link>
-//                 </span>
-//                 <span>
-//                   <Link to="#">
-//                     <i class="ri-facebook-circle-line"></i>
-//                   </Link>
-//                 </span>
-//                 <span>
-//                   <Link to="#">
-//                     <i class="ri-instagram-line"></i>
-//                   </Link>
-//                 </span>
-//               </div>
-//             </div>
-//           </Col>
-
-//           <Col lg="3">
-//             <h5 className="footer__link-title">Discover</h5>
-
-//             <ListGroup className="footer__quick-links">
-//               {quick__links.map((item, index) => (
-//                 <ListGroupItem key={index} className="ps-0 border-0">
-//                   <Link to={item.path}>{item.display}</Link>
-//                 </ListGroupItem>
-//               ))}
-//             </ListGroup>
-//           </Col>
-//           <Col lg="3">
-//             <h5 className="footer__link-title">Quick Links</h5>
-
-//             <ListGroup className="footer__quick-links">
-//               {quick__links2.map((item, index) => (
-//                 <ListGroupItem key={index} className="ps-0 border-0">
-//                   <Link to={item.path}>{item.display}</Link>
-//                 </ListGroupItem>
-//               ))}
-//             </ListGroup>
-//           </Col>
-//           <Col lg="3">
-//             <h5 className="footer__link-title">Contact</h5>
-
-//             <ListGroup className="footer__quick-links">
-//               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
-//                 <h6 className="mb-0 d-flex align-items-center gap-2">
-//                   <span>
-//                     <i class="ri-map-pin-line"></i>
-//                   </span>
-//                   Address:
-//                 </h6>
-//                 <p className="mb-0">Dwarka</p>
-//               </ListGroupItem>
-
-//               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
-//                 <h6 className="mb-0 d-flex align-items-center gap-2">
-//                   <span>
-//                     <i class="ri-mail-line"></i>
-//                   </span>
-//                   Email:
-//                 </h6>
-
-//                 <p className="mb-0">XYZ@gmail.com</p>
-//               </ListGroupItem>
-
-//               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
-//                 <h6 className="mb-0 d-flex align-items-center gap-2">
-//                   <span>
-//                     <i class="ri-phone-fill"></i>
-//                   </span>
-//                   Phone:
-//                 </h6>
-
-//                 <p className="mb-0">+91 0000000XYZ</p>
-//               </ListGroupItem>
-//             </ListGroup>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
 import React from "react";
 import "./footer.css";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
@@ -218,7 +81,7 @@ const Footer = () => {
                     </span>
                     Address:
                   </h6>
-                  <p className="mb-0">Dwarka</p>
+                  <p className="mb-0">.............</p>
                 </ListGroupItem>
 
                 <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
@@ -228,7 +91,7 @@ const Footer = () => {
                     </span>
                     Email:
                   </h6>
-                  <p className="mb-0">XYZ@gmail.com</p>
+                  <p className="mb-0">.......@.....com</p>
                 </ListGroupItem>
 
                 <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
@@ -238,7 +101,7 @@ const Footer = () => {
                     </span>
                     Phone:
                   </h6>
-                  <p className="mb-0">+91 0000000XYZ</p>
+                  <p className="mb-0">+91..........</p>
                 </ListGroupItem>
               </ListGroup>
             </div>
